8fba9bfb87cf038ce43c1fe87aaa86082d66b29a
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-21 03:12:02 -0500

    next.config.js to export @zeit/next-css;

--- src/Header.jsx
+++ src/Header.jsx
@@ -12,9 +12,19 @@ export const Header = () => {
         <div className="jumbotron jumbotronheight">
             <div className="row">
                 <div className="col-12 col-sm-4 text-center">
-                    <h6 className="text-uppercase"></h6>
+                    <h6 className="text-uppercase">October 19th&amp;20th&nbsp;&nbsp;2019</h6>
+                    <h6 className="text-uppercase">San Jose, California</h6>
+                </div>
+                <div className="col-12 col-sm-8 text-lg-right">
+                    <div>
+                        <img src="/static/SVCClogo.png" />
+                    </div>
+                    <h2>Silicon Valley Code Camp 2019</h2>
+                    <div className="row col-12 text-lg-right">
+                        <SignMeUp signupCallBack={signupCallBack} />
+                    </div>
                 </div>
             </div>
         </div>
     )
-}
+};
