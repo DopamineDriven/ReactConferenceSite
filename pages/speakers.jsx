8712cf286f4f04d8d625b262583e24f766622a7f
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-30 08:49:06 -0500

    updated speakers.jsx page

--- pages/speakers.jsx
+++ pages/speakers.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import App from '../src/App.jsx';
+import React from "react";
+import App from "../src/App.jsx";
+import ls from "local-storage";
 
-const speakers = () => {
-    return (
-        <App pageName="Speakers" />
-    )
+const Speakers = ({ user, isServer }) => {
+  const isBrowser = typeof window !== "undefined";
+
+  // this means running on first page load and inside the browser 
+  // so should store in local storage
+  if (isServer && isBrowser) {
+    ls.set("userInfo", user);
+  }
+  return (
+    <div>
+      <App pageName="Speakers" userInfo={user} />
+    </div>
+  );
 };
 
-export default speakers;
+Speakers.getInitialProps = async ({ req }) => {
+  const isServer = !!req;
+
+  if (isServer) {
+    return { user: req.user, isServer };
+  } else {
+    try {
+      const user = ls.get("userInfo");
+      return { user, isServer };
+    } catch (e) {
+      return { isServer };
+    }
+  }
+};
+
+export default Speakers;
