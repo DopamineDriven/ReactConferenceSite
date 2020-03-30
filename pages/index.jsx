d0f157d03cadbb95190899b862460ba0ac3f76da
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-30 08:23:16 -0500

    updates to index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import App from "../src/App";
 
-const index = () => {
+const Index = ({ user, isServer }) => {
+    const isBrowser = typeof window !== "undefined";
+
+    // this means running on first page load and 
+    // inside the browser so should store in local storage
+    if(isServer && isBrowser) {
+        ls.set("userInfo", user)
+    }
     return (
-        <App pageName="Home" />
+        <App pageName="Home" userInfo={user} />
     )
 };
 // this static method is run on the server side
 // inside node.js when the url is first hit
 // then again when the page renders on the client side 
-index.getInitialProps = async ({ req }) => {
+// when running SS, getInitialProps has access to user email
+// assuming user is authenticated
+Index.getInitialProps = async ({ req }) => {
     const isServer = !!req;
-    return { isServer }
+    
+    if (isServer) {
+        return { user: req.user, isServer }
+    } else {
+        try {
+            const user = ls.get("userInfo")
+            return { user, isServer }
+        }   catch (e) {
+            return { isServer }
+        }
+    }
 };
 
-export default index;
+export default Index;
