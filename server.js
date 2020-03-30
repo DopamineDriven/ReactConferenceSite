92ef14c5c2d58d4f5e0b51e4293461d962b97f81
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-30 08:09:54 -0500

    updates cont

--- server.js
+++ server.js
@@ -1,14 +1,58 @@
 const express = require('express');
 const next = require('next');
 
+// instatiate local passport strategy
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+
 const dev = process.env.NODE_ENV !== "production";
 const app1 = next({ dev });
 const handle = app1.getRequestHandler();
 const PORT = process.env.PORT || 3000;
+
+passport.use(
+    new Strategy((username, password, done) => {
+        const validateUser = (username, password) => {
+            return username === password
+        }
+
+        return validateUser(username, password)
+            ? done(null, { email: username })
+            : done(false, false); //done...sends to serialize
+    })
+);
+
+// serialize user
+passport.serializeUser((userInfo, done) => {
+    done(null, userInfo)
+});
+
+// deserialize user
+passport.deserializeUser((userInfo, cb) => {
+    cb(null, userInfo)
+});
+
+
 app1
     .prepare()
     .then(() => {
         const app = express();
+
+        // install everything passport needs between server postbacks
+        app.use(require("cookie-parser")());
+        app.use(require('body-parser').urlencoded({ extended: true }));
+        app.use(
+            require("express-session")({
+                secret: "keyboard cat",
+                resave: false,
+                saveUninitialized: false
+            })
+        );
+
+        // initialize passport and restore authentication state, if any, from the session
+        app.use(passport.initialize());
+        app.use(passport.session());
+
 
         app.get("*", (request, response) => {
             return handle(request, response)
@@ -16,6 +60,10 @@ app1
 
         app.listen(PORT, error => {
             if (error) throw (error)
-            console.log(`🌎 ==> API Server now listening on PORT http://localhost:${PORT}!`)
+            console.log(`🌎 ==> API Server now listening on PORT http://localhost:${PORT}`)
         })
     })
+    .catch(ex => {
+        console.error(ex.stack)
+        process.exit(1)
+});
